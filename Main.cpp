e4d9e90e357d0230fdf791d0afb3382beaee7b07
yoteru <warusura.yooteru@gmail.com>
2024-10-24 14:27:35 +0900

    ウイルスパニック！

---
+++ Main.cpp
@@ -0,0 +1,26 @@
+#include <DxLib.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	ChangeWindowMode(true);
+	SetGraphMode(800, 600, 32);
+	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
+	{
+		return -1;			// エラーが起きたら直ちに終了
+	}
+
+	while (ProcessMessage() == 0 &&
+		CheckHitKey(KEY_INPUT_ESCAPE) == 0)
+	{
+
+		
+
+		ClearDrawScreen();
+
+		ScreenFlip();
+	}
+
+	DxLib_End();			// ＤＸライブラリ使用の終了処理
+
+	return 0;				// ソフトの終了 
+}
